12f9445e2936864c51746fedecf40a9bebb19ab1
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-07-24 17:12:59 +0300

    add new test, new fixture(file1.yml ...)

--- __tests__/logic.test.js
+++ __tests__/logic.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import genDiff from '../src/logic.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,4 +14,10 @@ test('JSON files', () => {
   const actual1 = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
   const expected = readFile('result.txt');
   expect(actual1).toEqual(expected);
+});
+
+test('YAML files', () => {
+  const actual2 = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+  const expected = readFile('result.txt');
+  expect(actual2).toEqual(expected);
 });
