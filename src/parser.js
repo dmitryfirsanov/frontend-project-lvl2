0f563c4447923217ae304120b03d05117aef6c84
dmitryfirsanov03 <dmitryfirsanov03@gmail.com>
2022-07-24 17:10:57 +0300

    add parser, install js-yaml

---
+++ src/parser.js
@@ -0,0 +1,15 @@
+import yaml from 'js-yaml';
+
+const parser = (file, format) => {
+  switch (format) {
+    case '.json':
+      return JSON.parse(file);
+    case '.yml':
+    case '.yaml':
+      return yaml.load(file);
+    default:
+      throw new Error(`Unsupported format - ${format}`);
+  }
+};
+
+export default parser;
